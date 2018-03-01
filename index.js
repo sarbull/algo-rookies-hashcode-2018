1acecbf0364c83494ce50dfaa4d4136ae1086234
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-03-01 20:11:58 +0200

    Add read from file

---
+++ index.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var array = fs.readFileSync('problem.in').toString().split("\n");
+
+var problemInput = [];
+array.forEach(function(row, key) {
+  problemInput[key] = row.split(" ");
+});
+
+var rows = problemInput[0][0];
+var columns = problemInput[0][1];
+var vehicles = problemInput[0][2];
+var rides = problemInput[0][3];
+var bonus = problemInput[0][4];
+var steps = problemInput[0][5];
+
+console.log('rows = ', rows);
+console.log('columns = ', columns);
+console.log('vehicles = ', vehicles);
+console.log('rides = ', rides);
+console.log('bonus = ', bonus);
+console.log('steps = ', steps);
