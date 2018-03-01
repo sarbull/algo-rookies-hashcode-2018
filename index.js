ca9fb27ba2cda0446cbd392bf1a89acef63665f1
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-03-01 21:17:52 +0200

    Latest version

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ array.forEach(function(row, key) {
   problemInput[key] = row.split(" ");
 });
 
-var rowsInput = parseInt(problemInput[0][0]);
-var columnsInput = parseInt(problemInput[0][1]);
-var vehiclesInput = parseInt(problemInput[0][2]);
-var ridesInput = parseInt(problemInput[0][3]);
-var bonusInput = parseInt(problemInput[0][4]);
-var stepsInput = parseInt(problemInput[0][5]);
+var input = {
+  rows: parseInt(problemInput[0][0]),
+  columns: parseInt(problemInput[0][1]),
+  vehicles: parseInt(problemInput[0][2]),
+  rides: parseInt(problemInput[0][3]),
+  bonus: parseInt(problemInput[0][4]),
+  steps: parseInt(problemInput[0][5])
+};
 
 var rides = [];
 
-for(var i = 0; i < ridesInput; i++) {
+for(var i = 0; i < input.rides; i++) {
   var r = problemInput[i+1];
 
   var ride = {
@@ -37,23 +39,44 @@ rides.sort(function(a, b) {
   return a.steps > b.steps;
 });
 
-var map = new Array(rowsInput);
-for(var ii = 0; ii < rowsInput; ii++) {
-  map[ii] = new Array(columnsInput);
+var map = new Array(input.rows);
+for(var ii = 0; ii < input.rows; ii++) {
+  map[ii] = new Array(input.columns);
 }
 
-console.log('rowsInput = ', rowsInput);
-console.log('columnsInput = ', columnsInput);
-console.log('vehiclesInput = ', vehiclesInput);
-console.log('ridesInput = ', ridesInput);
-console.log('bonusInput = ', bonusInput);
-console.log('stepsInput = ', stepsInput);
-console.log('rides = ', rides);
+console.log('input.rows = ', input.rows);
+console.log('input.columns = ', input.columns);
+console.log('input.vehicles = ', input.vehicles);
+console.log('input.rides = ', input.rides);
+console.log('input.bonus = ', input.bonus);
+console.log('input.steps = ', input.steps);
+// console.log('rides = ', rides);
+
+var finisedVehiclesRides = [];
 
 function iteration(step) {
+  input.vehicles--; console.log('input.vehicles = ', input.vehicles);
 
+  rides.forEach(function(ride) {
+    if(input.vehicles > 0) {
+      if(step === ride.startStep) {
+        ride.steps--;
+
+        // if(ride.steps === 0) {
+        //
+        //   finisedVehiclesRides.push({finished: ride});
+        //
+        //   input.vehicles++;
+        // }
+      } else {
+        // input.vehicles++;
+      }
+    }
+  });
 }
 
-for(var step = 0; step <= stepsInput; step++) {
+for(var step = 1; step <= input.steps; step++) {
   iteration(step);
 }
+
+console.log(finisedVehiclesRides);
