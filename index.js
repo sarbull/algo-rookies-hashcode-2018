fae1a9e3a6a5c8cfb10f154db6942580b54ea076
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-03-01 20:43:59 +0200

    Add iteration and sort rides by number of steps

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ array.forEach(function(row, key) {
   problemInput[key] = row.split(" ");
 });
 
-var rowsInput = problemInput[0][0];
-var columnsInput = problemInput[0][1];
-var vehiclesInput = problemInput[0][2];
-var ridesInput = problemInput[0][3];
-var bonusInput = problemInput[0][4];
-var stepsInput = problemInput[0][5];
+var rowsInput = parseInt(problemInput[0][0]);
+var columnsInput = parseInt(problemInput[0][1]);
+var vehiclesInput = parseInt(problemInput[0][2]);
+var ridesInput = parseInt(problemInput[0][3]);
+var bonusInput = parseInt(problemInput[0][4]);
+var stepsInput = parseInt(problemInput[0][5]);
 
 var rides = [];
 
@@ -20,22 +20,27 @@ for(var i = 0; i < ridesInput; i++) {
   var r = problemInput[i+1];
 
   var ride = {
-    x: r[0],
-    y: r[1],
-    xTo: r[2],
-    yTo: r[3],
-    startStep: r[4],
-    stopStep: r[5]
+    x: parseInt(r[0]),
+    y: parseInt(r[1]),
+    xTo: parseInt(r[2]),
+    yTo: parseInt(r[3]),
+    startStep: parseInt(r[4]),
+    stopStep: parseInt(r[5])
   };
+
+  ride.steps = Math.abs(ride.xTo - ride.x) + Math.abs(ride.yTo - ride.y);
 
   rides.push(ride);
 }
 
-var map = new Array(parseInt(rowsInput));
-for(var ii = 0; ii < rowsInput; ii++) {
-  map[ii] = new Array(parseInt(columnsInput));
-}
+rides.sort(function(a, b) {
+  return a.steps > b.steps;
+});
 
+var map = new Array(rowsInput);
+for(var ii = 0; ii < rowsInput; ii++) {
+  map[ii] = new Array(columnsInput);
+}
 
 console.log('rowsInput = ', rowsInput);
 console.log('columnsInput = ', columnsInput);
@@ -45,6 +50,10 @@ console.log('bonusInput = ', bonusInput);
 console.log('stepsInput = ', stepsInput);
 console.log('rides = ', rides);
 
-for(var step = 0; step <= stepsInput; step++) {
+function iteration(step) {
 
+}
+
+for(var step = 0; step <= stepsInput; step++) {
+  iteration(step);
 }
